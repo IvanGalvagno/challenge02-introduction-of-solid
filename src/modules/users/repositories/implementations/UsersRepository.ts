a2f4cc8ee44f5edf05597b851ce5271bb2d5a3ca
Ivan Galvagno <Ivan.Galvagno@gmail.com>
2021-09-05 15:21:03 -0300

    find user by email

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -35,7 +35,7 @@ class UsersRepository implements IUsersRepository {
   }
 
   findByEmail(email: string): User | undefined {
-    // Complete aqui
+    return this.users.find((user) => user.email === email);
   }
 
   turnAdmin(receivedUser: User): User {
